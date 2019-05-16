c53a9f9c9072c82317a8457b2d19e3a0ccaeed61
sfaisal25 <saad.faisal25@gmail.com>
2019-05-16 10:07:14 -0400

    Added LoginSystem to project

--- Planner/src/planner/Planner.java
+++ Planner/src/planner/Planner.java
@@ -1,19 +1,11 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+HEADER
  */
 package planner;
 
-/**
- *
- * @author S331464230
- */
 public class Planner {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
         // TODO code application logic here
     }

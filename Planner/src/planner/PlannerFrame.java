5c43de7e5462b453b058ab77f6f1f192d55b5869
sfaisal25 <saad.faisal25@gmail.com>
2019-05-28 10:09:15 -0400

    Getting date and new jFrame created

--- Planner/src/planner/PlannerFrame.java
+++ Planner/src/planner/PlannerFrame.java
@@ -4,21 +4,94 @@ package planner;
 HEADER
 */
 
+import java.awt.BorderLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import javax.swing.*;
+
 public class PlannerFrame extends javax.swing.JFrame {
     public PlannerFrame() {
         initComponents();
+        LocalDateTime myDateObj = LocalDateTime.now();
+        DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("E. MMM dd, yyyy");
+        String formattedDate = myDateObj.format(myFormatObj);
+        //System.out.println(formattedDate);
+        dateLabel = new JLabel(formattedDate);
+        dateLabel.setBounds(10, 10, 100, 20);
+        dashPane.setLayout(null);
+        dashPane.add(dateLabel);
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        newEventFrame = new javax.swing.JFrame();
+        jLabel1 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        jTextField2 = new javax.swing.JTextField();
+        jLabel3 = new javax.swing.JLabel();
+        jTextField3 = new javax.swing.JTextField();
         tabbedPane = new javax.swing.JTabbedPane();
         dashPane = new javax.swing.JPanel();
         addEventButton = new javax.swing.JButton();
-        dateLabel = new javax.swing.JLabel();
         calendarPane = new javax.swing.JPanel();
         notesPane = new javax.swing.JPanel();
+
+        jLabel1.setText("Event Title");
+
+        jTextField1.setText("jTextField1");
+
+        jLabel2.setText("Event Descripion");
+
+        jTextField2.setText("jTextField2");
+
+        jLabel3.setText("Event Date");
+
+        jTextField3.setText("jTextField3");
+
+        javax.swing.GroupLayout newEventFrameLayout = new javax.swing.GroupLayout(newEventFrame.getContentPane());
+        newEventFrame.getContentPane().setLayout(newEventFrameLayout);
+        newEventFrameLayout.setHorizontalGroup(
+            newEventFrameLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(newEventFrameLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(newEventFrameLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(newEventFrameLayout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addGap(18, 18, 18)
+                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(newEventFrameLayout.createSequentialGroup()
+                        .addComponent(jLabel2)
+                        .addGap(18, 18, 18)
+                        .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(newEventFrameLayout.createSequentialGroup()
+                        .addComponent(jLabel3)
+                        .addGap(18, 18, 18)
+                        .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(233, Short.MAX_VALUE))
+        );
+        newEventFrameLayout.setVerticalGroup(
+            newEventFrameLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(newEventFrameLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(newEventFrameLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(newEventFrameLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(newEventFrameLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(200, Short.MAX_VALUE))
+        );
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -26,16 +99,12 @@ public class PlannerFrame extends javax.swing.JFrame {
 
         addEventButton.setText("ADD NEW EVENT");
 
-        dateLabel.setText("jLabel1");
-
         javax.swing.GroupLayout dashPaneLayout = new javax.swing.GroupLayout(dashPane);
         dashPane.setLayout(dashPaneLayout);
         dashPaneLayout.setHorizontalGroup(
             dashPaneLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, dashPaneLayout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(dateLabel)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 608, Short.MAX_VALUE)
+            .addGroup(dashPaneLayout.createSequentialGroup()
+                .addContainerGap(672, Short.MAX_VALUE)
                 .addComponent(addEventButton)
                 .addContainerGap())
         );
@@ -43,10 +112,8 @@ public class PlannerFrame extends javax.swing.JFrame {
             dashPaneLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(dashPaneLayout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(dashPaneLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(addEventButton)
-                    .addComponent(dateLabel))
-                .addContainerGap(491, Short.MAX_VALUE))
+                .addComponent(addEventButton)
+                .addContainerGap(538, Short.MAX_VALUE))
         );
 
         tabbedPane.addTab("Dashboard", dashPane);
@@ -55,11 +122,11 @@ public class PlannerFrame extends javax.swing.JFrame {
         calendarPane.setLayout(calendarPaneLayout);
         calendarPaneLayout.setHorizontalGroup(
             calendarPaneLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 775, Short.MAX_VALUE)
+            .addGap(0, 795, Short.MAX_VALUE)
         );
         calendarPaneLayout.setVerticalGroup(
             calendarPaneLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 525, Short.MAX_VALUE)
+            .addGap(0, 572, Short.MAX_VALUE)
         );
 
         tabbedPane.addTab("Calendar", calendarPane);
@@ -68,11 +135,11 @@ public class PlannerFrame extends javax.swing.JFrame {
         notesPane.setLayout(notesPaneLayout);
         notesPaneLayout.setHorizontalGroup(
             notesPaneLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 775, Short.MAX_VALUE)
+            .addGap(0, 795, Short.MAX_VALUE)
         );
         notesPaneLayout.setVerticalGroup(
             notesPaneLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 525, Short.MAX_VALUE)
+            .addGap(0, 572, Short.MAX_VALUE)
         );
 
         tabbedPane.addTab("Notes", notesPane);
@@ -119,7 +186,7 @@ public class PlannerFrame extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(PlannerFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
-
+        
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new PlannerFrame().setVisible(true);
@@ -130,8 +197,15 @@ public class PlannerFrame extends javax.swing.JFrame {
     private javax.swing.JButton addEventButton;
     private javax.swing.JPanel calendarPane;
     private javax.swing.JPanel dashPane;
-    private javax.swing.JLabel dateLabel;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
+    private javax.swing.JTextField jTextField3;
+    private javax.swing.JFrame newEventFrame;
     private javax.swing.JPanel notesPane;
     private javax.swing.JTabbedPane tabbedPane;
     // End of variables declaration//GEN-END:variables
+    private JLabel dateLabel;
 }

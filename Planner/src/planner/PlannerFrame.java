b785be82678979ee2131b8b9484186e4445b03f4
sfaisal25 <saad.faisal25@gmail.com>
2019-06-05 10:43:28 -0400

    Manually merged calendarPane branch and master branch

--- Planner/src/planner/PlannerFrame.java
+++ Planner/src/planner/PlannerFrame.java
@@ -1,17 +1,13 @@
 package planner;
 
-import java.awt.Color;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.event.ActionEvent;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -19,7 +15,25 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.*;
 import javax.swing.border.LineBorder;
+import java.awt.Container;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.GregorianCalendar;
 import javax.swing.colorchooser.AbstractColorChooserPanel;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
 
 public class PlannerFrame extends javax.swing.JFrame {
     public List<String> oldEvents;
@@ -27,10 +41,86 @@ public class PlannerFrame extends javax.swing.JFrame {
     public List<JPanel> eventPanels = new ArrayList<>();
     public String user = LoginSystem.user;
     public int tmpIndex;
+    private int calYear, calMonth, calDay, currentYear, currentMonth;
     
     public PlannerFrame() {
         initComponents();
         addDefaultC();
+        
+        lblMonth = new JLabel ("January");
+        labelText =new JLabel ("ERROR");
+        lblYear = new JLabel ("Change year:");
+        cmbYear = new JComboBox();
+        btnPrev = new JButton ("Previous");
+        btnNext = new JButton ("Next");
+        mtblCalendar = new DefaultTableModel(){
+        @Override
+        public boolean isCellEditable(int rowIndex, int mColIndex){return false;}};
+        tblCalendar = new JTable(mtblCalendar);
+        stblCalendar = new JScrollPane(tblCalendar);
+        pnlCalendar = new JPanel(null);
+        outputLabel = new javax.swing.JFrame();
+        pnlCalendar.setBorder(BorderFactory.createTitledBorder("Calendar"));
+        calendarPane.setLayout (null);
+        calendarPane.add(lblMonth, BorderLayout.PAGE_START);
+          setResizable(false);
+
+        btnPrev.addActionListener(new btnPrev_Action());
+        btnNext.addActionListener(new btnNext_Action());
+        cmbYear.addActionListener(new cmbYear_Action());
+        
+        //Add controls to pane
+        calendarPane.add(lblMonth);
+        calendarPane.add(lblYear);
+        calendarPane.add(cmbYear);
+        calendarPane.add(btnPrev);
+        calendarPane.add(btnNext);
+        calendarPane.add(stblCalendar);
+         calendarPane.setBackground(new Color(255, 229, 153));
+
+        calendarPane.setBounds(0, 0, calendarPane.getWidth(), calendarPane.getHeight());
+        lblMonth.setBounds(160-lblMonth.getPreferredSize().width/2, 25, 100, 25);
+        lblYear.setBounds(575, 450, 80, 20);
+        cmbYear.setBounds(650, 450, 80, 20);
+        btnPrev.setBounds(100, 25, 100, 25);
+        btnNext.setBounds(600, 25, 100, 25);
+        stblCalendar.setBounds(0, 50, calendarPane.getWidth(), calendarPane.getHeight());
+
+
+         GregorianCalendar cal = new GregorianCalendar(); //Create calendar
+        calDay = cal.get(GregorianCalendar.DAY_OF_MONTH); //Get day
+        calMonth = cal.get(GregorianCalendar.MONTH); //Get month
+        calYear = cal.get(GregorianCalendar.YEAR); //Get year
+        currentMonth = calMonth; //Match month and year
+        currentYear = calYear;
+        
+        String[] headers = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"}; //All headers
+        for (int i=0; i<7; i++){
+            mtblCalendar.addColumn(headers[i]);
+        }
+        
+                tblCalendar.setBackground(new Color(255, 229, 153));
+                setBackground(new Color(255, 229, 153));
+
+        
+        //No resize/reorder
+        tblCalendar.getTableHeader().setResizingAllowed(false);
+        tblCalendar.getTableHeader().setReorderingAllowed(false);
+        
+        //Single cell selection
+        tblCalendar.setColumnSelectionAllowed(true);
+        tblCalendar.setRowSelectionAllowed(true);
+        tblCalendar.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        
+        //Set row/column count
+        tblCalendar.setRowHeight(60);
+        mtblCalendar.setColumnCount(7);
+        mtblCalendar.setRowCount(6);
+        
+        //Populate table
+        for (int i=calYear; i<=calYear+1; i++){
+            cmbYear.addItem(String.valueOf(i));
+        }
         
         newColorChooser.setPreviewPanel(new JPanel());
         AbstractColorChooserPanel[] oldPanels = newColorChooser.getChooserPanels();
@@ -45,6 +135,8 @@ public class PlannerFrame extends javax.swing.JFrame {
         editColorChooser.removeChooserPanel(oldPanels2[2]);
         editColorChooser.removeChooserPanel(oldPanels2[4]);
         editEventFrame.pack();
+        
+        refreshCalendar (calMonth, calYear);
     }
 
     @SuppressWarnings("unchecked")
@@ -409,6 +501,125 @@ public class PlannerFrame extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    public void refreshCalendar(int month, int year){
+        //Variables
+        String[] months =  {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+        int nod, som; //Number Of Days, Start Of Month
+        
+        //Allow/disallow buttons
+        if (currentYear==calYear && currentMonth==0){
+            btnPrev.setEnabled(false);
+        }
+        else{
+        btnPrev.setEnabled(true);
+        }
+        if (currentYear==2020 && currentMonth==11){
+            btnNext.setEnabled(false);
+        }
+        else{
+        btnNext.setEnabled(true);
+        }
+        if (month == 0 && year <= calYear-10){
+            btnPrev.setEnabled(false);
+        } //Too early
+        else if (month == 11 && year >= calYear+100){
+            btnNext.setEnabled(false);
+        } //Too late
+        lblMonth.setText(months[month]); //Refresh the month label (at the top)
+        lblMonth.setBounds(400, 25, 180, 25); //Re-align label with calendar
+        cmbYear.setSelectedItem(String.valueOf(year)); //Select the correct year in the combo box
+        
+        //Clear table
+        for (int i=0; i<6; i++){
+            for (int j=0; j<7; j++){
+                mtblCalendar.setValueAt(null, i, j);
+            }
+        }
+        
+        //Get first day of month and number of days
+        GregorianCalendar cal = new GregorianCalendar(year, month, 1);
+        nod = cal.getActualMaximum(GregorianCalendar.DAY_OF_MONTH);
+        som = cal.get(GregorianCalendar.DAY_OF_WEEK);
+        
+        //Draw calendar
+        for (int i=1; i<=nod; i++){
+            int row = (i+som-2)/7;
+            int column  =  (i+som-2)%7;
+            mtblCalendar.setValueAt(i, row, column);
+        }
+        
+        
+        //Apply renderers
+        tblCalendar.setDefaultRenderer(tblCalendar.getColumnClass(0), new tblCalendarRenderer());
+    }
+    
+    class tblCalendarRenderer extends DefaultTableCellRenderer{
+        public Component getTableCellRendererComponent (JTable table, Object value, boolean selected, boolean focused, int row, int column){
+            super.getTableCellRendererComponent(table, value, selected, focused, row, column);
+            if (column == 0 || column == 6){ //Week-end
+                setBackground(new Color(255, 220, 220));
+            }
+            else{ //Week
+                setBackground(new Color(255, 229, 153));
+            }
+            if (value != null){
+                if (Integer.parseInt(value.toString()) == calDay && currentMonth == calMonth && currentYear == calYear){ //Today
+                    setBackground(new Color(220, 220, 255));
+                }
+            }
+            refreshCalendar(currentMonth, currentYear);    
+            setBorder(null);
+            setForeground(Color.black);
+            return this;
+        }
+    }
+    
+    class btnPrev_Action implements ActionListener{
+        public void actionPerformed (ActionEvent e){
+            if (currentMonth == 0){ //Back one year
+                currentMonth = 11;
+                currentYear -= 1;
+            }
+            else{ //Back one month
+                currentMonth -= 1;
+            }
+            refreshCalendar(currentMonth, currentYear);
+        }
+    }
+    
+    private void btnPrevActionPerformed(java.awt.event.ActionEvent evt) {                                         
+            if (currentMonth == 0){ //Back one year
+                currentMonth = 11;
+                currentYear -= 1;
+            }
+            else{ //Back one month
+                currentMonth -= 1;
+            }
+            refreshCalendar(currentMonth, currentYear);    
+    }
+    
+    class btnNext_Action implements ActionListener{
+        public void actionPerformed (ActionEvent e){
+            if (currentMonth == 11){ //Foward one year
+                currentMonth = 0;
+                currentYear += 1;
+            }
+            else{ //Foward one month
+                currentMonth += 1;
+            }
+            refreshCalendar(currentMonth, currentYear);
+        }
+    }
+    class cmbYear_Action implements ActionListener{
+        public void actionPerformed (ActionEvent e){
+            if (cmbYear.getSelectedItem() != null){
+                String b = cmbYear.getSelectedItem().toString();
+                currentYear = Integer.parseInt(b);
+                refreshCalendar(currentMonth, currentYear);
+            }
+        }
+    }
+    
     private void addEventButtonActionPerformed(java.awt.event.ActionEvent evt) {
         newTitleField.setText("");
         newDescField.setText("");
@@ -668,4 +879,14 @@ public class PlannerFrame extends javax.swing.JFrame {
     // End of variables declaration//GEN-END:variables
     private JLabel dateLabel;
     private JButton addEventButton;
+    private JLabel lblMonth, lblYear, labelText;
+    private JButton btnPrev, btnNext;
+    private JTable tblCalendar;
+    private JComboBox cmbYear;
+    private JFrame frmMain;
+    private Container pane;
+    private DefaultTableModel mtblCalendar; //Table model
+    private JScrollPane stblCalendar; //The scrollpane
+    private JPanel pnlCalendar;
+    private JFrame outputLabel;
 }
